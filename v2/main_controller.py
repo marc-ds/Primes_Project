3081330e495ec5a62745e375eb0ecedef29c17e2
marcelo <marcelo1982@gmail.com>
2018-09-24 23:58:03 -0300

    Set the P1P2P3Line class that return an iterator with the values.

--- v2/main_controller.py
+++ v2/main_controller.py
@@ -11,7 +11,7 @@ def roundz(f):
 
 def y_vertex(a, b):
     if a != 0:
-        return -b/ (2.*a)
+        return -b / (2.*a)
     else:
         return 00
 
@@ -30,31 +30,31 @@ class P1P2P3:
         self.p1 = int(p1)
         self.p2 = int(p2)
         self.p3 = int(p3)
-        self.a = (self.p1-(2*self.p2)+self.p3)/2.
-        self.b = (self.p3-self.p1)/2.
-        self.c = self.p2
-        self.y_vertex = y_vertex(self.a, self.b)
-        self.offset = offset(self.a, self.b)
+        self.a_value = (self.p1-(2*self.p2)+self.p3)/2.
+        self.b_value = (self.p3-self.p1)/2.
+        self.c_value = self.p2
+        self.y_vertex = y_vertex(self.a_value, self.b_value)
+        self.offset = offset(self.a_value, self.b_value)
 
     def __call__(self, y):
         """return the value of the f(x) = ay^2 + by + c"""
-        return int(self.a * y ** 2 + self.b * y + self.c)
+        return int(self.a_value * y ** 2 + self.b_value * y + self.c_value)
 
 class P10P20P30(P1P2P3):
     """define the result of function P1P2P3 when the offset is 0"""
     def __init__(self,  p1, p2, p3):
         super().__init__(p1, p2, p3)
-        self.a0 = abs(self.a)
-        self.b0 = -abs(self.b + 2 * self.a * self.offset)
-        if self.a <= 0:
-            self.c0 = -(self.c + self.a * self.offset)
+        self.a0 = abs(self.a_value)
+        self.b0 = -abs(self.b_value + 2 * self.a_value * self.offset)
+        if self.a_value <= 0:
+            self.c0 = -(self.c_value + self.a_value * self.offset)
         else:
-            self.c0 = self.c + self.a * self.offset
+            self.c0 = self.c_value + self.a_value * self.offset
         self.y_vertex = y_vertex(self.a0, self.b0)
         self.offset = offset(self.a0, self.b0)
 
 
-class P1P2P3Line(P1P2P3):
+class Paraboctys(P1P2P3):
     """return an iterable with a line in a given range """
     def __init__(self, p1, p2, p3, p1_step=0, p2_step=0, p3_step=1, init=-20, end=20):
         self.p1 = int(p1)
@@ -72,7 +72,7 @@ class P1P2P3Line(P1P2P3):
         b_line = self.b()
         c_line = self.c()
         for i in self.p_range:
-            yield int(a_line.__next__() * y ** 2 + b_line.__next__() * y + c_line.__next__())
+            yield int(a_line.__next__() * y ** 2 + b_line.__next__() * y + c_line.__next__()) # change to send an P1P2P3 object instead int
 
     def a(self):
         for i in self.p_range:
@@ -91,16 +91,14 @@ class P1P2P3Line(P1P2P3):
 
     def y_vertex(self):
         for i in self.p_range:
-            a = ((self.p1 + (self.p1_step * i)) - 2 * (self.p2 + (self.p2_step * i)) + (
-                        self.p3 + (self.p3_step * i))) / 2.
-            b = ((self.p3 + (self.p3_step * i)) - (self.p1 + (self.p1_step * i))) / 2.
+            a = self.a()
+            b = self.b()
             yield y_vertex(a, b)
 
     def offset(self):
         for i in self.p_range:
-            a = ((self.p1 + (self.p1_step * i)) - 2 * (self.p2 + (self.p2_step * i)) + (
-                    self.p3 + (self.p3_step * i))) / 2.
-            b = ((self.p3 + (self.p3_step * i)) - (self.p1 + (self.p1_step * i))) / 2.
+            a = self.a()
+            b = self.b()
             yield offset(a, b)
 
 
@@ -111,7 +109,7 @@ def column(obj, init, end):
 
 a = P1P2P3(41,43,47)
 b = P10P20P30(41,43,47)
-c = P1P2P3Line(41,43,45)
+c = Paraboctys(41,43,45)
 print('a:{:g}, b:{:g}, c:{:g}, yv:{:g}, f:{:g}, result at y=0: {}'.format(a.a, a.b, a.c, a.y_vertex, a.offset, a(0)))
 print('a0:{}, b0:{}, c0:{}, yv0:{}, f0:{}, result at y=0: {}'.format(b.a, b.b, b.c, b.y_vertex, b.offset, b(0)))
 print('Column a={}'.format(list(column(a, -3, 3))))
