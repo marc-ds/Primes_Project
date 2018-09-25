a8c032056213212a7758fb6abbc106350d168690
marcelo <marcelo1982@gmail.com>
2018-09-25 00:56:08 -0300

    Refactored almost all. Now Paraboctys and P1P2P3 class working fine and do not need offset0 version.

--- v2/main_controller.py
+++ v2/main_controller.py
@@ -25,36 +25,36 @@ def offset(a,b):
 
 class P1P2P3:
     """by P1, P2 and P3 set the values of a, b, c, y_vertex and offset, when called return f(x) = ay^2 + by + c"""
-    def __init__(self, p1, p2, p3):
+    def __init__(self, p1, p2, p3, y):
         """set a, b and c"""
         self.p1 = int(p1)
         self.p2 = int(p2)
         self.p3 = int(p3)
-        self.a_value = (self.p1-(2*self.p2)+self.p3)/2.
-        self.b_value = (self.p3-self.p1)/2.
-        self.c_value = self.p2
-        self.y_vertex = y_vertex(self.a_value, self.b_value)
-        self.offset = offset(self.a_value, self.b_value)
-
-    def __call__(self, y):
-        """return the value of the f(x) = ay^2 + by + c"""
-        return int(self.a_value * y ** 2 + self.b_value * y + self.c_value)
-
-class P10P20P30(P1P2P3):
-    """define the result of function P1P2P3 when the offset is 0"""
-    def __init__(self,  p1, p2, p3):
-        super().__init__(p1, p2, p3)
-        self.a0 = abs(self.a_value)
-        self.b0 = -abs(self.b_value + 2 * self.a_value * self.offset)
-        if self.a_value <= 0:
-            self.c0 = -(self.c_value + self.a_value * self.offset)
+        self.y = int(y)
+        self.a = (self.p1-(2*self.p2)+self.p3)/2.
+        self.b = (self.p3-self.p1)/2.
+        self.c = self.p2
+        self.y_vertex = y_vertex(self.a, self.b)
+        self.offset = offset(self.a, self.b)
+        self.a0 = abs(self.a)
+        self.b0 = -abs(self.b + 2 * self.a * self.offset)
+        if self.a <= 0:
+            self.c0 = -(self.c + self.a * self.offset)
         else:
-            self.c0 = self.c_value + self.a_value * self.offset
-        self.y_vertex = y_vertex(self.a0, self.b0)
-        self.offset = offset(self.a0, self.b0)
+            self.c0 = self.c + self.a * self.offset
+        self.y_vertex0 = y_vertex(self.a0, self.b0)
+        self.offset0 = offset(self.a0, self.b0)
+
+    def __call__(self):
+        """return the value of f(x) = ay^2 + by + c"""
+        return int(self.a * self.y ** 2 + self.b * self.y + self.c)
+
+    def when_f0(self):
+        """return the value of f(x) = ay^2 + by + c when offset is 0"""
+        return int(self.a0 * self.y ** 2 + self.b0 * self.y + self.c0)
 
 
-class Paraboctys(P1P2P3):
+class Paraboctys():
     """return an iterable with a line in a given range """
     def __init__(self, p1, p2, p3, p1_step=0, p2_step=0, p3_step=1, init=-20, end=20):
         self.p1 = int(p1)
@@ -68,50 +68,13 @@ class Paraboctys(P1P2P3):
         self.p_range = range(self.init, self.end+1)
 
     def __call__(self, y):
-        a_line = self.a()
-        b_line = self.b()
-        c_line = self.c()
         for i in self.p_range:
-            yield int(a_line.__next__() * y ** 2 + b_line.__next__() * y + c_line.__next__()) # change to send an P1P2P3 object instead int
-
-    def a(self):
-        for i in self.p_range:
-            a = ((self.p1 + (self.p1_step*i)) - 2 * (self.p2 + (self.p2_step*i)) + (self.p3 + (self.p3_step*i))) / 2.
-            yield a
-
-    def b(self):
-        for i in self.p_range:
-            b = ((self.p3 + (self.p3_step*i)) - (self.p1 + (self.p1_step*i))) / 2.
-            yield b
-
-    def c(self):
-        for i in self.p_range:
-            c = self.p2 + (self.p2_step*i)
-            yield c
-
-    def y_vertex(self):
-        for i in self.p_range:
-            a = self.a()
-            b = self.b()
-            yield y_vertex(a, b)
-
-    def offset(self):
-        for i in self.p_range:
-            a = self.a()
-            b = self.b()
-            yield offset(a, b)
+            p1 = self.p1 + (self.p1_step*i)
+            p2 = self.p2 + self.p2_step*i
+            p3 = self.p3 + (self.p3_step*i)
+            yield P1P2P3(p1, p2, p3, y)
 
 
-def column(obj, init, end):
-    for y in range(end, init-1, -1):
-        yield obj(y)
-
-
-a = P1P2P3(41,43,47)
-b = P10P20P30(41,43,47)
 c = Paraboctys(41,43,45)
-print('a:{:g}, b:{:g}, c:{:g}, yv:{:g}, f:{:g}, result at y=0: {}'.format(a.a, a.b, a.c, a.y_vertex, a.offset, a(0)))
-print('a0:{}, b0:{}, c0:{}, yv0:{}, f0:{}, result at y=0: {}'.format(b.a, b.b, b.c, b.y_vertex, b.offset, b(0)))
-print('Column a={}'.format(list(column(a, -3, 3))))
-print('a:{}\nb:{}\nc:{}\nyv:{}\nf:{}\nresult at y=0: {}\nrange:{}'.format(list(c.a()), list(c.b()), list(c.c()), list(c.y_vertex()), list(c.offset()), 'qqq', c.p_range))
-print('{}'.format(list(c(2))))
+for i in c(3):
+    print(i())
