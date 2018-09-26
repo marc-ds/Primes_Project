3100a20223a71e482ce5c49610496ecb54b90ab7
marcelo <marcelo1982@gmail.com>
2018-09-26 00:09:39 -0300

    Sequence seeker view almost finished, need only the factored composites first/last in each sequence

--- v2/main_controller.py
+++ v2/main_controller.py
@@ -139,6 +139,54 @@ class SequenceSeeker():
                 yv_seq.append(y_vertex(a, b))
         return yv_seq, offset_seq, abc_seq, big_seq
 
+    def when_f0(self, min_size):
+        big_seq = list()
+        abc_seq = list()
+        yv_seq = list()
+        offset_seq = list()
+        previous_prime = int()
+        for k in self.k_range:
+            primes_seq = list()
+            p1, p2, p3 = (self.p1 + (self.p1_step * k)), self.p2 + (self.p2_step * k), self.p3 + (self.p3_step * k)
+            a = (p1 - (2 * (p2)) + p3) / 2.
+            b = (p3 - p1) / 2.
+            c = p2
+            f = offset(a, b)
+            a0 = abs(a)
+            b0 = -abs(b + 2 * a * f)
+            if a <= 0:
+                c0 = -(c + a * f)
+            else:
+                c0 = c + a * f
+            yp = 1
+            yn = 0
+            possible_prime = p1p2p3(a0, b0, c0, yp)
+            while isprime(possible_prime):
+                primes_seq.append(possible_prime)
+                yp += 1
+                p_previous_prime = previous_prime
+                previous_prime = possible_prime
+                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, yp): break
+                possible_prime = p1p2p3(a0, b0, c0, yp)
+            primes_seq.append(possible_prime)
+            primes_seq.reverse()
+            possible_prime = p1p2p3(a0, b0, c0, yn)
+            while isprime(possible_prime):
+                primes_seq.append(possible_prime)
+                yn -= 1
+                p_previous_prime = previous_prime
+                previous_prime = possible_prime
+                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, yn): break
+                possible_prime = p1p2p3(a0, b0, c0, yn)
+            primes_seq.append(possible_prime)
+            if len(primes_seq) > min_size:
+                big_seq.append(primes_seq)
+                abc_seq.append([a0, b0, c0])
+                offset_seq.append(offset(a0, b0))
+                yv_seq.append(y_vertex(a0, b0))
+        return yv_seq, offset_seq, abc_seq, big_seq
+
+
 """
 c = Paraboctys(41,43,45)
 for i in c(3):
--- v2/sequenceSeeker_view.py
+++ v2/sequenceSeeker_view.py
@@ -1,65 +1,214 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import cgi
 import cgitb
-from math import floor, sqrt
+from math import sqrt
 from main_controller import *
-
-print('Content-type: text/html\r\n\r')
-print('<html>')
-print('<head>')
-print('<link rel="stylesheet" type="text/css" href="/styles/style.css">')
-print('<title>Sequences table</title>')
-print('</head>')
-print('<body>')
-
+cgitb.enable()
 form = cgi.FieldStorage()
 
-p1 = float(form["p1"].value)
-p2 = float(form["p2"].value)
-p3 = float(form["p3"].value)
-p1_step = float(form["p1_step"].value)
-p2_step = float(form["p2_step"].value)
-p3_step = float(form["p3_step"].value)
-k = int(form["k"].value)
 
-calc = SequenceSeeker(p1, p2, p3, p1_step, p2_step, p3_step, k)
-yv_seq, off_seq, abc_seq, big_seq = calc(min_size)
-print('<table id=sequence_seeker>')
 
-for i in range(0, len(big_seq)):
-    first = int(big_seq[i].pop(0))
-    last = int(big_seq[i].pop())
-    print('<tr class="sequence_seeker_header">')
-    yv_txt = '<th class="sequence_seeker" id="{}" >y_vertex: {:1.4g}</th>'
-    yv = yv_seq[i]
-    if yv < 0:
-        print(yv_txt.format(y_vertex_negative, yv))
-    elif yv > 0:
-        print(yv_txt.format(y_vertex_positive, yv))
-    elif yv == 0:
-        print(yv_txt.format(y_vertex_zero, yv))
-    else:
-        print(yv_txt.format(no_y_vertex, yv))
-    off_txt = '<th class="sequence_seeker" id="{}" >offset: {:1.4g}</th>'
-    f = off_seq[i]
-    if f < 0:
-        print(off_txt.format(offset_negative, f))
-    elif f > 0:
-        print(off_txt.format(offset_positive, f))
-    elif f == 0:
-        print(off_txt.format(offset_zero, f))
-    else:
-        print(off_txt.format(no_offset, f))
-    abc = abc_seq[i]
-    a = abc[0]
-    print('<th class="sequence_seeker" id="a" >a: {:g.0}</th>'.format(a))
-    b = abc[0]
-    print('<th class="sequence_seeker" id="b" >b: {:g.0}</th>'.format(b))
-    c = abc[0]
-    print('<th class="sequence_seeker" id="c" >c: {:g.0}</th>'.format(c))
-    length = len(big_seq[i]) - big_seq[i].count(1) - big_seq[i].count(-1)
-    print('<th class="sequence_seeker" id="qtd_primes" >Primes in the above sequence: {}.').format(length)
+if "p1" not in form or "p2" not in form or "p3" not in form:
+
+    print('Content-type: text/html\r\n\r')
+    print('<html>')
+    print('<head>')
+    print('<link rel="stylesheet" type="text/css" href="/styles/style.css">')
+    print('<title>Sequences table</title>')
+    print('</head>')
+    print('<body>')
+    print('<form action="/v2/sequenceSeeker_view.py" method="post" id="sequence_seeker">')
+    print('<div id="p_value">P1 initial value = <input type="text" name="p1" value="" />')
+    print('P2 initial value &nbsp;= <input type="text" name="p2" value="" />')
+    print('P3 initial value = <input type="text" name="p3" value="" /></div>')
+    print('<div id="p_step">P1 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p1_step" value="0" />')
+    print('P2 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p2_step" value="0" />')
+    print('P3 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p3_step" value="1" /></div>')
+    print('<div class="others" id="k">Columns range = <input type="text" name="k" value="100" />')
+    print('Minimum size of first sequences = <input type="text" name="min_size" value="7" id="min_value"/>')
+    print('<input type="submit" value="Generate" id="submit"/></div></form>')
+    print('<h1 id="sequence_seeker">Please fill the fields</h1>')
+    print('</body>')
+    print('</html>')
+
+else:
+
+    p1 = float(form["p1"].value)
+    p2 = float(form["p2"].value)
+    p3 = float(form["p3"].value)
+    p1_step = float(form["p1_step"].value)
+    p2_step = float(form["p2_step"].value)
+    p3_step = float(form["p3_step"].value)
+    k = int(form["k"].value)
+    min_size = int(form["min_size"].value)
+
+    print('Content-type: text/html\r\n\r')
+    print('<html>')
+    print('<head>')
+    print('<link rel="stylesheet" type="text/css" href="/styles/style.css">')
+    print('<title>Sequences table</title>')
+    print('</head>')
+    print('<body>')
+    print('<form action="/v2/sequenceSeeker_view.py" method="post" id="sequence_seeker">')
+    print('<div id="p_value">P1 initial value = <input type="text" name="p1" value="{:g}" />'.format(p1))
+    print('P2 initial value &nbsp;= <input type="text" name="p2" value="{:g}" />'.format(p2))
+    print('P3 initial value = <input type="text" name="p3" value="{:g}" /></div>'.format(p3))
+    print('<div id="p_step">P1 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p1_step" value="{:g}" />'.format(p1_step))
+    print('P2 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p2_step" value="{:g}" />'.format(p2_step))
+    print('P3 step value &nbsp;&nbsp;&nbsp;= <input type="text" name="p3_step" value="{:g}" /></div>'.format(p3_step))
+    print('<div class="others" id="k">Columns range = <input type="text" name="k" value="{}" />'.format(k))
+    print('Minimum size of first sequences = <input type="text" name="min_size" value="{}" id="min_value"/>'.format(min_size))
+    print('<input type="submit" value="Generate" id="submit"/></div></form>')
+
+
+    calc = SequenceSeeker(p1, p2, p3, p1_step, p2_step, p3_step, k)
+    yv_seq, off_seq, abc_seq, big_seq = calc(min_size)
+    yv0_seq, off0_seq, abc0_seq, big0_seq = calc.when_f0(min_size)
+    print('<h1 id=sequence_seeker >{} sequences found.</h1>'.format(len(big_seq)))
+    print('<table id=sequence_seeker>')
+
+    for i in range(0, len(big_seq)):
+        first = int(big_seq[i].pop(0))
+        last = int(big_seq[i].pop())
+        print('<tr class="sequence_seeker_header">')
+        yv_txt = '<td class="sequence_seeker" id="{}" >y_vertex: {:1.4g}</td>'
+        yv = yv_seq[i]
+        if yv < 0:
+            print(yv_txt.format('y_vertex_negative', yv))
+        elif yv > 0:
+            print(yv_txt.format('y_vertex_positive', yv))
+        elif yv == 0:
+            print(yv_txt.format('y_vertex_zero', yv))
+        else:
+            print(yv_txt.format('no_y_vertex', yv))
+        off_txt = '<td class="sequence_seeker" id="{}" >offset: {:1.4g}</th>'
+        f = off_seq[i]
+        if f < 0:
+            print(off_txt.format('offset_negative', f))
+        elif f > 0:
+            print(off_txt.format('offset_positive', f))
+        elif f == 0:
+            print(off_txt.format('offset_zero', f))
+        else:
+            print(off_txt.format('no_offset', f))
+        abc = abc_seq[i]
+        a = abc[0]
+        b = abc[1]
+        c = abc[2]
+        delta = int(b ** 2 - 4 * a * c)
+        sqrtdelta = sqrt(abs(delta))
+        c_g = sqrtdelta - int(sqrtdelta)
+
+        if abs(delta) is 1:
+            print('<td class="sequences_table" id="delta_one">&Delta; {:.4g}</td>'.format(delta))
+        elif delta == 0:
+            print('<td class="sequences_table" id="delta_zero">&Delta; {:.4g}</td>'.format(delta))
+        elif isprime(delta):
+            print('<td class="sequences_table" id="delta_prime">&Delta; {:.4g}</td>'.format(delta))
+        elif not (sqrt(abs(delta)) * 10) % 2:
+            print('<td class="sequences_table" id="delta_sqrt">&Delta; {:.4g}</td>'.format(delta))
+        else:
+            print('<td class="sequences_table" id="delta_composite">&Delta; {:.4g}</td>'.format(delta))
+        if abs(delta) is 1:
+            print('<td class="sequences_table" id="c_g_one">C.G. {:.4g}</td>'.format(c_g))
+        elif delta == 0:
+            print('<td class="sequences_table" id="c_g_zero">C.G. {:.4g}</td>'.format(c_g))
+        elif isprime(delta):
+            print('<td class="sequences_table" id="c_g_prime">C.G. {:.4g}</td>'.format(c_g))
+        elif not (sqrt(abs(delta)) * 10) % 2:
+            print('<td class="sequences_table" id="c_g_sqrt_round">C.G. {:.4g}</td>'.format(c_g))
+        else:
+            print('<td class="sequences_table" id="c_g_composite">C.G. {:.4g}</td>'.format(c_g))
+
+        print('<td class="sequence_seeker" id="a" >a: {:g}</th>'.format(a))
+        print('<td class="sequence_seeker" id="b" >b: {:g}</th>'.format(b))
+        print('<td class="sequence_seeker" id="c" >c: {:g}</th>'.format(c))
+        length = len(big_seq[i]) - big_seq[i].count(1) - big_seq[i].count(-1)
+        print('<td class="sequence_seeker" id="qtd_primes" >#P 1st Seq: {}.'.format(length))
+        for result in big_seq[i]:
+            if abs(result) is 1:
+                print('<td class="sequences_table" id="one">{}</td>'.format(result))
+            elif result == 0:
+                print('<td class="sequences_table" id="zero">{}</td>'.format(result))
+            elif isprime(result):
+                print('<td class="sequences_table" id="prime">{}</td>'.format(result))
+            elif not (sqrt(abs(result)) * 10) % 2:
+                print('<td class="sequences_table" id="sqrt_round">{}</td>'.format(result))
+            else:
+                print('<td class="sequences_table" id="composite">{}</td>'.format(result))
+        print('</tr>')
+
+    for i in range(0, len(big0_seq)):
+        first = int(big0_seq[i].pop(0))
+        last = int(big0_seq[i].pop())
+        print('<tr class="sequence_seeker_header">')
+        yv_txt = '<td class="sequence_seeker" id="{}" >y_vertex&ordm: {:1.4g}</td>'
+        yv = yv0_seq[i]
+        if yv < 0:
+            print(yv_txt.format('y_vertex_negative', yv))
+        elif yv > 0:
+            print(yv_txt.format('y_vertex_positive', yv))
+        elif yv == 0:
+            print(yv_txt.format('y_vertex_zero', yv))
+        else:
+            print(yv_txt.format('no_y_vertex', yv))
+        off_txt = '<td class="sequence_seeker" id="{}" >offset&ordm: {:1.4g}</th>'
+        f = off0_seq[i]
+        if f < 0:
+            print(off_txt.format('offset_negative', f))
+        elif f > 0:
+            print(off_txt.format('offset_positive', f))
+        elif f == 0:
+            print(off_txt.format('offset_zero', f))
+        else:
+            print(off_txt.format('no_offset', f))
+        abc = abc0_seq[i]
+        a = abc[0]
+        b = abc[1]
+        c = abc[2]
+        delta = int(b**2-4*a*c)
+        sqrtdelta = sqrt(abs(delta))
+        c_g = sqrtdelta - int(sqrtdelta)
+
+        if abs(delta) is 1:
+            print('<td class="sequences_table" id="one">&Delta;&ordm {:.4g}</td>'.format(delta))
+        elif delta == 0:
+            print('<td class="sequences_table" id="zero">&Delta;&ordm {:.4g}</td>'.format(delta))
+        elif isprime(delta):
+            print('<td class="sequences_table" id="prime">&Delta;&ordm {:.4g}</td>'.format(delta))
+        elif not (sqrt(abs(delta)) * 10) % 2:
+            print('<td class="sequences_table" id="sqrt_round">&Delta;&ordm {:.4g}</td>'.format(delta))
+        else:
+            print('<td class="sequences_table" id="composite">&Delta;&ordm {:.4g}</td>'.format(delta))
+        if abs(delta) is 1:
+            print('<td class="sequences_table" id="one">C.G. {:.4g}</td>'.format(c_g))
+        elif delta == 0:
+            print('<td class="sequences_table" id="zero">C.G. {:.4g}</td>'.format(c_g))
+        elif isprime(delta):
+            print('<td class="sequences_table" id="prime">C.G. {:.4g}</td>'.format(c_g))
+        elif not (sqrt(abs(delta)) * 10) % 2:
+            print('<td class="sequences_table" id="sqrt_round">C.G. {:.4g}</td>'.format(c_g))
+        else:
+            print('<td class="sequences_table" id="composite">C.G. {:.4g}</td>'.format(c_g))
+
+        print('<td class="sequence_seeker" id="a" >a&ordm: {:g}</th>'.format(a))
+        print('<td class="sequence_seeker" id="b" >b&ordm: {:g}</th>'.format(b))
+        print('<td class="sequence_seeker" id="c" >c&ordm: {:g}</th>'.format(c))
+        length = len(big0_seq[i]) - big0_seq[i].count(1) - big0_seq[i].count(-1)
+        print('<td class="sequence_seeker" id="qtd_primes" >#P 1st Seq: {}.'.format(length))
+        for result in big0_seq[i]:
+            if abs(result) is 1:
+                print('<td class="sequences_table" id="one">{}</td>'.format(result))
+            elif result == 0:
+                print('<td class="sequences_table" id="zero">{}</td>'.format(result))
+            elif isprime(result):
+                print('<td class="sequences_table" id="prime">{}</td>'.format(result))
+            elif not (sqrt(abs(result)) * 10) % 2:
+                print('<td class="sequences_table" id="sqrt_round">{}</td>'.format(result))
+            else:
+                print('<td class="sequences_table" id="composite">{}</td>'.format(result))
+        print('</tr>')
 
 print('</body>')
 print('</html>')
