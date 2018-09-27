919f341bc0a01430c71589823dfb818f0c6eb986
Marcelo D. Silva <marcelo1982@gmail.com>
2018-09-27 14:03:23 -0300

    change the way to check if values are the same when looking for a sequence.

--- v2/sequenceSeeker_controller.py
+++ v2/sequenceSeeker_controller.py
@@ -2,14 +2,14 @@ from main_controller import *
 
 class SequenceSeeker():
     """seek first primes sequences of given P1, P2, P3, p_steps and columns range (k)"""
-    def __init__(self, p1, p2, p3, p1_step=0, p2_step=0, p3_step=1, k=10):
+    def __init__(self, p1, p2, p3, p1_step=0, p2_step=0, p3_step=1, kn=10):
         self.p1 = int(p1)
         self.p2 = int(p2)
         self.p3 = int(p3)
         self.p1_step = int(p1_step)
         self.p2_step = int(p2_step)
         self.p3_step = int(p3_step)
-        self.k_range = range(-int(k)+1, int(k)+1)
+        self.k_range = range(-int(kn), int(kn)+1)
 
     def __call__(self, min_size=3):
         big_seq = list()
@@ -20,29 +20,25 @@ class SequenceSeeker():
         for k in self.k_range:
             primes_seq = list()
             p1, p2, p3 =(self.p1 + (self.p1_step*k)), self.p2 + (self.p2_step*k), self.p3 + (self.p3_step*k)
-            a = (p1 - (2 * (p2)) + p3) / 2.
-            b = (p3 - p1) / 2.
+            a = (p1 - 2.0 * p2 + p3) / 2.0
+            b = (p3 - p1) / 2.0
             c = p2
-            yp = 1
-            yn = 0
-            possible_prime = p1p2p3(a, b, c, yp)
+            ypos = 1
+            yneg = 0
+            possible_prime = p1p2p3(a, b, c, ypos)
             while isprime(possible_prime):
+                if a == b and b == c: break;
                 primes_seq.append(possible_prime)
-                yp += 1
-                p_previous_prime = previous_prime
-                previous_prime = possible_prime
-                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a, b, c, yp): break
-                possible_prime = p1p2p3(a, b, c, yp)
+                ypos += 1
+                possible_prime = p1p2p3(a, b, c, ypos)
             primes_seq.append(possible_prime)
             primes_seq.reverse()
-            possible_prime = p1p2p3(a, b, c, yn)
+            possible_prime = p1p2p3(a, b, c, yneg)
             while isprime(possible_prime):
+                if a == b and b == c: break;
                 primes_seq.append(possible_prime)
-                yn -= 1
-                p_previous_prime = previous_prime
-                previous_prime = possible_prime
-                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a, b, c, yn): break
-                possible_prime = p1p2p3(a, b, c, yn)
+                yneg -= 1
+                possible_prime = p1p2p3(a, b, c, yneg)
             primes_seq.append(possible_prime)
             if (len(primes_seq)-1) > min_size:
                 big_seq.append(primes_seq)
@@ -70,26 +66,26 @@ class SequenceSeeker():
                 c0 = -(c + a * f)
             else:
                 c0 = c + a * f
-            yp = 1
-            yn = 0
-            possible_prime = p1p2p3(a0, b0, c0, yp)
+            ypos = 1
+            yneg = 0
+            possible_prime = p1p2p3(a0, b0, c0, ypos)
             while isprime(possible_prime):
                 primes_seq.append(possible_prime)
-                yp += 1
+                ypos += 1
                 p_previous_prime = previous_prime
                 previous_prime = possible_prime
-                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, yp): break
-                possible_prime = p1p2p3(a0, b0, c0, yp)
+                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, ypos): break
+                possible_prime = p1p2p3(a0, b0, c0, ypos)
             primes_seq.append(possible_prime)
             primes_seq.reverse()
-            possible_prime = p1p2p3(a0, b0, c0, yn)
+            possible_prime = p1p2p3(a0, b0, c0, yneg)
             while isprime(possible_prime):
                 primes_seq.append(possible_prime)
-                yn -= 1
+                yneg -= 1
                 p_previous_prime = previous_prime
                 previous_prime = possible_prime
-                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, yn): break
-                possible_prime = p1p2p3(a0, b0, c0, yn)
+                if p_previous_prime == previous_prime and previous_prime == p1p2p3(a0, b0, c0, yneg): break
+                possible_prime = p1p2p3(a0, b0, c0, yneg)
             primes_seq.append(possible_prime)
             if (len(primes_seq)-1) > min_size:
                 big_seq.append(primes_seq)
