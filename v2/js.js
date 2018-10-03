482c86ac4a97a16c62f1ef02f388f15c14c3eb67
marcelo <marcelo1982@gmail.com>
2018-10-03 09:32:05 -0300

    Change the way to set the range

--- v2/js.js
+++ v2/js.js
@@ -1,11 +1,20 @@
-a, b, c = self.a, self.b, self.c
-        f = self.offset
-        _a0 = a
-        _b0 = b + 2 * (a * f)
-        _c0 = c + a * (f ** 2) + (b * f)
-        self.a0 = abs(_a0)
-        self.b0 = -abs(_b0)
-        if _a0 <= 0:
-            self.c0 = -(_c0)
-        else:
-            self.c0 = _c0
+
+function sendform() {
+    var formElement = document.querySelector("form");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/v2/sequenceSeekerV2_view.py");
+    xhr.timeout = 4294967295;
+
+    function carregou() {document.getElementById("loader").style.visibility='visible';}
+
+    xhr.onload = function () {
+
+        document.getElementById("loader").style.visibility='hidden';
+        document.getElementById("sequence_seeker").innerHTML = xhr.responseText;
+
+    };
+
+    xhr.send(new FormData(formElement));
+    document.onload = carregou();
+
+}
