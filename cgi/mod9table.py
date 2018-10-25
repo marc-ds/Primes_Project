a7670099457450ece3803535123aadbf7d10888d
Marcelo D. Silva <marcelo1982@gmail.com>
2018-10-25 20:58:31 -0300

    add 0 as first column set mod9 table cell as square

--- cgi/mod9table.py
+++ cgi/mod9table.py
@@ -58,24 +58,29 @@ def triangle_ns(n_value, s_value, columns_number, rows_number):
 @app.route('/triangle-nc/N<n_value>s<s_value>/<columns_number>/<rows_number>')
 def triangle_nc(n_value, s_value, columns_number, rows_number):
 
-    range_cols = range(0, int(columns_number))
-    range_rows = range(2, int(rows_number))
+    range_cols = range(0, int(columns_number))  # range das colunas.
+    range_rows = range(2, int(rows_number))     # range das linhas.
     rows = [list(), list()]
 
     for i in range_cols:
-        rows[0].append(int(s_value))
-        rows[1].append(int(n_value)+int(s_value))
+        rows[0].append(int(s_value))                # faz a primeira linha só com elementos 0.
+        if i == 0:
+            rows[1].append(0)
+        else:
+            rows[1].append(int(n_value)+int(s_value))   # faz a segunda linha so com elementos 1.
 
-    for r in range_rows:
-        row = []
-        for c in range_cols:
-            if c == 0:
+    for r in range_rows:  # loop de qtd de linhas.
+        row = []  # limpa a linha a cada iteração.
+        for c in range_cols:  # loop de qtd de colunas.
+            if c == 0:                  # verifica se é a primeira coluna,
+                value = int(s_value)    # se for define valor como 1.
+            elif c ==1:
                 value = int(n_value)
             else:
-                value = row[c-1] + rows[r-1][c]
-            row.append(value)
+                value = row[c-1] + rows[r-1][c]  # soma o elemento anterior com o elemento da mesma posicão da linha acima.
+            row.append(value)  # adiciona o valor a linha atual.
 
-        rows.append(row)
+        rows.append(row)  # adiciona a linha atual ao conjunto de linhas.
 
     return render_template('triangle-nc.html', rows=rows)
 
