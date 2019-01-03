dfc2b1e1ba75c273cc72aff89b1966af634d987a
Marcelo D. Silva <marcelo1982@gmail.com>
2019-01-03 07:12:00 -0200

    final ssedb

--- cgi/ssedb.py
+++ cgi/ssedb.py
@@ -19,19 +19,15 @@ for p1 in p1_range:
 
     if engine == 'v5':
         p2_range = rpup_positive(int(p1), rangep2)
-        print('---Using V5------------V5')
     elif engine == 'v6':
         p2_range = rpdown_positive(int(p1), rangep2)
-        print('---Using V6------------V6')
 
     for p2 in p2_range:
         p3_range = rpup_positive(int(p2), rangep3)
         if engine == 'v6':
             p3_range = rpup_positive(int(p2), rangep3)
-            print('---Using V6------------V6')
         elif engine == 'v5':
             p3_range = rpdown_positive(int(p2), rangep3)
-            print('---Using V5------------V5')
 
         for p3 in p3_range:
 
@@ -51,10 +47,9 @@ for p1 in p1_range:
             reg = c.fetchone()
             try:
                 if type(reg[0]) is int:
-                    print('--Já salvo: {}, P1={}, P2={}, P3={}--'.format(formula, obj_x.p1, obj_x.p2, obj_x.p3))
                     continue
             except:
-                print('Nova formula: {}, P1={}, P2={}, P3={}'.format(formula, obj_x.p1, obj_x.p2, obj_x.p3))
+                nada = 2
 
             possible_prime = x(p1, p2, p3, 0)
             if isprime(possible_prime):
@@ -66,70 +61,29 @@ for p1 in p1_range:
             yn = -1
             density = float(0)
 
-            if obj_x.par_type is 'ACC':
+            possible_prime = x(p1, p2, p3, yp)
 
+            while isprime(possible_prime):
+                sequence_size += 1
+                yp += 1
                 possible_prime = x(p1, p2, p3, yp)
 
-                while isprime(possible_prime):
-                    sequence_size += 1
-                    yp += 1
-                    possible_prime = x(p1, p2, p3, yp)
+            possible_prime = x(p1, p2, p3, yn)
 
+            while isprime(possible_prime):
+                sequence_size += 1
+                yn -= 1
                 possible_prime = x(p1, p2, p3, yn)
 
-                while isprime(possible_prime):
-                    sequence_size += 1
-                    yn -= 1
-                    possible_prime = x(p1, p2, p3, yn)
-
-                if sequence_size <= min_size:
-                    print('DESCARTADO --- {} - Tam. seq. {}'.format(formula, sequence_size))
-                    continue
-
-                #density_pos = obj_x.density_pos(density_range)
-                #density_neg = obj_x.density_neg(density_range)
-                #density = (density_neg + density_pos) / (density_range * 2)
-
-            elif obj_x.par_type is 'DES':
-
-                possible_prime = x(p1, p2, p3, yp)
-                while isprime(possible_prime):
-                    sequence_size += 1
-                    yp += 1
-                    possible_prime = x(p1, p2, p3, yp)
-
-                sequence_size = sequence_size * 2
-
-                if sequence_size <= min_size:
-                    print('DESCARTADO - DES --- {} - Tam. seq. {}'.format(formula, sequence_size))
-                    continue
-
-                #density_pos = obj_x.density_pos(density_range)
-                #density = density_pos / density_range
-
-            elif obj_x.par_type is 'SUB':
-
-                possible_prime = x(p1, p2, p3, yp)
-                while isprime(possible_prime):
-                    sequence_size += 1
-                    yp += 1
-                    possible_prime = x(p1, p2, p3, yp)
-
-                sequence_size = (sequence_size * 2) - 1
-
-                if sequence_size <= min_size:
-                    print('DESCARTADO - SUB --- {} - Tam. seq. {}'.format(formula, sequence_size))
-                    continue
-
-                #density_pos = obj_x.density_pos(density_range)
-                #density = density_pos / density_range
+            if sequence_size <= min_size:
+                continue
 
             try:
                 c.execute('INSERT INTO xzero (formula, sequence_size, density) VALUES (?,?,?)',
                           (formula, sequence_size, density))
                 con.commit()
 
-                print('SALVO --- {}'.format(formula))
+                print('Salvo {}, do tipo {} com {} elementos'.format(formula, obj_x.par_type, sequence_size))
 
             except:
                 print('Erro ao salvar a formula, tamanho da sequencia ou densidade')
